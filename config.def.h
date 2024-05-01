42f134c6370235d2b64a86b23dc0df5a3146e66e
augustotx <augusto.teixeira03@proton.me>
2024-05-01 00:04:19 -0300

    gcc instead of c99

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=16";
+static char *font = "JetBrains Mono:pixelsize=14";
 /* Spare fonts */
 static char *font2[] = {
 	"Symbols Nerd Font:pixelsize=14"
